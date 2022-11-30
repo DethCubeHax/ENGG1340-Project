49148a5db3909adeae9c1333301b0a148f8522cd
alx-chn <alxchn1026@gmail.com>
2022-11-30 22:42:37 +0800

    Replaced [SIZE] with [SIZE*SIZE] and Did the printBoard() and Added a line in main() to call printBoard()

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,32 +1,39 @@
+#include <iostream>
 #include "Sudoku.h"
 #include "GlobalVars.h"
+using namespace std;
 
-bool Sudoku::numberIsPossible(int number, int x, int y, int (&board)[SIZE])
+bool Sudoku::numberIsPossible(int number, int x, int y, int (&board)[SIZE*SIZE])
 {
     if (checkBlock(number, x, y, board) && checkColumn(number, x, y, board) && checkColumn(number, x, y, board)) return true;
 }
 
-void Sudoku::printBoard(int (&board)[SIZE])
+void Sudoku::printBoard(int (&board)[SIZE*SIZE])
+{
+    for ( int i = 0 ; i < SIZE ; i++ ){
+        for ( int j = 0 ; j < SIZE ; j++ ){
+            cout<<board[i+j]<<' ';
+        }
+        cout<<endl;
+    }
+}
+
+void Sudoku::readBoard(int (&board)[SIZE*SIZE])
 {
 
 }
 
-void Sudoku::readBoard(int (&board)[SIZE])
+bool Sudoku::checkRow(int number, int x, int y, int (&board)[SIZE*SIZE])
 {
 
 }
 
-bool Sudoku::checkRow(int number, int x, int y, int (&board)[SIZE])
+bool Sudoku::checkColumn(int number, int x, int y, int (&board)[SIZE*SIZE])
 {
 
 }
 
-bool Sudoku::checkColumn(int number, int x, int y, int (&board)[SIZE])
-{
-
-}
-
-bool Sudoku::checkBlock(int number, int x, int y, int (&board)[SIZE])
+bool Sudoku::checkBlock(int number, int x, int y, int (&board)[SIZE*SIZE])
 {
 
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Sudoku.h"
 #include "GlobalVars.h"
+using namespace std;
+Sudoku sudoku;
 
 int player1Board[SIZE*SIZE] = {0};
 int player2Board[SIZE*SIZE] = {0};
 
 int main()
 {
-    
+    sudoku.printBoard(player1Board);
 }
