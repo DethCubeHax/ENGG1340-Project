256b50e336cd1d7ef9cbf5c47a0f4d701fcf4550
alx-chn <alxchn1026@gmail.com>
2022-12-01 00:38:51 +0800

    Completed checkRow(), checkColumn(), checkBlock() in Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -44,15 +44,57 @@ void Sudoku::readBoard(int (&board)[SIZE][SIZE])
 
 bool Sudoku::checkRow(int number, int x, int y, int (&board)[SIZE][SIZE])
 {
-
+    bool notIn = true;
+    for ( int i = 0 ; i < SIZE ; i++){
+        if ( board[x][i] == number ){
+            notIn = false;
+        }
+    }
+    return notIn;
 }
 
 bool Sudoku::checkColumn(int number, int x, int y, int (&board)[SIZE][SIZE])
 {
-
+    bool notIn = true;
+    for ( int i = 0 ; i < SIZE ; i++){
+        if ( board[i][y] == number ){
+            notIn = false;
+        }
+    }
+    return notIn;
 }
 
 bool Sudoku::checkBlock(int number, int x, int y, int (&board)[SIZE][SIZE])
 {
+    int startRow = 0, startColumn = 0;
+    if ( 0 <= x <= 2 ){
+        startRow = 0;
+    }
+    else if ( 3 <= x <= 5 ){
+        startRow = 3;
+    }
+    else{
+        startRow = 6;
+    }
 
+    if ( 0 <= y <= 2 ){
+        startColumn = 0;
+    }
+    else if ( 3 <= y <= 5 ){
+        startColumn = 3;
+    }
+    else{
+        startColumn = 6;
+    }
+    
+    bool notIn = true;
+    for ( int i = startRow ; i < startRow+3 ; i++){
+        for ( int j = startColumn ; j < startColumn+3 ; j++){
+            if ( board[i][j] == number ){
+            notIn = false;
+            break;
+            }
+        }
+    }
+    return notIn;
 }
